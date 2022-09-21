0d8311bc5d4528847ff20bd8c619c67f3fae7c24
Guy-Jacques Isombe <isombeg@mcmaster.ca>
2022-09-21 17:22:09 -0400

    Automate lab1 (#4)
    * started modifications
    
    * still making adjustments
    
    * Added more outputs trying to figure out why sim_rslts dont output anything
    
    * Sim results are displaying nothing
    
    * fixed results storing issue
    
    * adding csv writing
    
    * Step 6 added
    
    * Step 8
    
    * writes to csv now
    
    * stufff
    
    * adding to gitignore
    
    * time
    
    * remove a.out
    
    * rm csv
    
    * finished automation
    
    Co-authored-by: Carlos Capili <carloscapili21@gmail.com>
    Co-authored-by: MouthOpenMonkey <59804756+CarlosCapili@users.noreply.github.com>

--- lab1/coe4dk4_lab_1_2022.c
+++ lab1/coe4dk4_lab_1_2022.c
@@ -28,6 +28,19 @@
 
 /*******************************************************************************/
 
+typedef struct {
+  double arrival_rate;
+  int service_time;
+  double number_to_serve;
+  double max_queue_size;
+
+  double utilization;
+  double fraction_served;
+  double rejection_probability;
+  double mean_number_in_system;
+  double mean_delay;
+} Simulation_Result;
+
 /*
  * Simulation Parameters
  */
@@ -40,6 +53,362 @@
 
 #define BLIP_RATE 10000
 
+#define MAX_QUEUE_SIZE 20e6 //20 million queue for now we can change this 
+#define MAX_QUEUE_SIZES_SIZE_DEFAULT 1
+
+typedef struct {
+  char* step_name;
+
+  double* numbers_to_serve;
+  int numbers_to_serve_size;
+
+  int* service_times;
+  int service_times_size;
+
+  double* arrival_rates;
+  int arrival_rates_size;
+
+  int use_max_queue;
+  double* max_queue_sizes;
+  int max_queue_sizes_size;
+
+  int* random_seeds;
+  // int random_seeds_size;
+
+  int md1_or_mm1; // 0: M/D/1, 1: M/M/1
+
+  // TODO: Add additional parameters to handle steps 7 and 8
+} Step_Configuration;
+
+double MAX_QUEUE_SIZES_DEFAULT[] = {MAX_QUEUE_SIZE};
+
+double step2_numbers_to_serve[] = {NUMBER_TO_SERVE};
+int step2_service_times[] = {10};
+double step2_arrival_rates[] = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09};
+int step2_random_seeds[] = {RANDOM_SEED, 400137394, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED};
+
+double step3_numbers_to_serve[] = {10e3, 20e3, 30e3, 40e3, 50e3, 60e3};
+int step3_service_times[] = {10};
+double step3_arrival_rates[] = {1.1};
+int step3_random_seeds[] = {RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, 400137394, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED}; 
+
+double step4_numbers_to_serve[] = {NUMBER_TO_SERVE};
+int step4_service_times[] = {30};
+double step4_arrival_rates[] = {0.01, 0.04, 0.07, 0.11, 0.14, 0.17, 0.20, 0.23, 0.26, 0.29};
+int step4_random_seeds[] = {RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED, 400137394, RANDOM_SEED, RANDOM_SEED, RANDOM_SEED}; 
+
+double step7_arrival_rates[] = {0.01};
+double step7_max_queue_sizes[] = {3, 6, 12, 25, 50, 10e1};
+const int step7_max_queue_sizes_size = 6;
+
+double step8_arrival_rates[] = {1/0.01, 1/0.02, 1/0.03, 1/0.04, 1/0.05, 1/0.06, 1/0.07, 1/0.08, 1/0.09};
+
+Step_Configuration steps[] = {
+  {
+    "Step 2",
+    step2_numbers_to_serve,
+    1,
+    step2_service_times,
+    1,
+    step2_arrival_rates,
+    9,
+    0,
+    MAX_QUEUE_SIZES_DEFAULT,
+    MAX_QUEUE_SIZES_SIZE_DEFAULT,
+    step2_random_seeds,
+    0,
+  },
+  {
+    "Step 3",
+    step3_numbers_to_serve,
+    6,
+    step3_service_times,
+    1,
+    step3_arrival_rates,
+    1,
+    0,
+    MAX_QUEUE_SIZES_DEFAULT,
+    MAX_QUEUE_SIZES_SIZE_DEFAULT,
+    step3_random_seeds,
+    0,
+  },
+  {
+    "Step 4",
+    step4_numbers_to_serve,
+    1,
+    step4_service_times,
+    1,
+    step4_arrival_rates,
+    10,
+    0,
+    MAX_QUEUE_SIZES_DEFAULT,
+    MAX_QUEUE_SIZES_SIZE_DEFAULT,
+    step4_random_seeds,
+    0,
+  },
+  {
+    "Step 6",
+    step2_numbers_to_serve,
+    1,
+    step2_service_times,
+    1,
+    step2_arrival_rates,
+    9,
+    0,
+    MAX_QUEUE_SIZES_DEFAULT,
+    MAX_QUEUE_SIZES_SIZE_DEFAULT,
+    step2_random_seeds,
+    1,
+  },
+  {
+    "Step 7",
+    step2_numbers_to_serve,
+    1,
+    step2_service_times,
+    1,
+    step7_arrival_rates,
+    1,
+    1,
+    step7_max_queue_sizes,
+    step7_max_queue_sizes_size,
+    step2_random_seeds,
+    1,
+  },
+  {
+    "Step 8",
+    step2_numbers_to_serve,
+    1,
+    step2_service_times,
+    1,
+    step8_arrival_rates,
+    9,
+    0,
+    MAX_QUEUE_SIZES_DEFAULT,
+    MAX_QUEUE_SIZES_SIZE_DEFAULT,
+    step2_random_seeds,
+    0,
+  }
+};
+
+int run(Step_Configuration *steps, int steps_size);
+int run_step(Step_Configuration step, FILE *fpt);
+Simulation_Result run_simulation(
+  double number_to_serve,
+  int service_time,
+  double arrival_rate,
+  int* random_seeds,
+  int md1_or_mm1,
+  int use_max_queue,
+  double max_queue_size
+);
+int flush_results(char* step_name, Simulation_Result *sim_rslts, int sim_rslts_size, FILE *fpt);
+
+int run(Step_Configuration *steps, int steps_size){
+ //printf("MADE IT TO RUN!\n");
+  FILE *fpt = fopen("simulation_results.csv", "w");
+  fprintf(fpt, "Arrival Rate, Service Time, Number to Serve, Utilization, Fraction Served, Customer Rejection Probability, Mean Number in System, Mean Delay\n");
+
+  for(int step_index = 0; step_index < steps_size; step_index++){
+    printf("\nSimulating %s\n", steps[step_index].step_name);
+    run_step(steps[step_index], fpt);
+  }
+
+  fclose(fpt);
+
+  return 0;
+}
+
+int run_step(Step_Configuration step, FILE *fpt){
+  //printf("MADE IT TO RUN_STEP!\n");
+  int sim_rslts_size = step.numbers_to_serve_size * step.service_times_size * step.arrival_rates_size * step.max_queue_sizes_size;
+  Simulation_Result* sim_rslts = calloc(
+    sim_rslts_size,
+    sizeof(Simulation_Result)
+  );
+
+
+  for(int i = 0; i < step.numbers_to_serve_size; i++){
+    for(int j = 0; j < step.service_times_size; j++){
+      for(int k = 0; k < step.arrival_rates_size; k++){
+          for(int l = 0; l < step.max_queue_sizes_size; l++){
+          sim_rslts[i*step.service_times_size + j*step.arrival_rates_size +k*step.max_queue_sizes_size + l] = run_simulation(
+            step.numbers_to_serve[i],
+            step.service_times[j],
+            step.arrival_rates[k],
+            step.random_seeds,
+            step.md1_or_mm1,
+            step.use_max_queue,
+            step.max_queue_sizes[l]
+          );
+        }
+      }
+    }
+  }
+
+  flush_results(step.step_name, sim_rslts, sim_rslts_size, fpt);
+
+  return 0;
+}
+
+Simulation_Result run_simulation(
+  double number_to_serve,
+  int service_time,
+  double arrival_rate,
+  int* random_seeds,
+  int md1_or_mm1,
+  int use_max_queue,
+  double max_queue_size
+){
+  //printf("RUN_SIMULATION!\n");
+  double acc_utilization = 0;
+  double acc_fraction_served = 0;
+  double acc_mean_number_in_system = 0;
+  double acc_mean_delay = 0;
+  double acc_rejection_probability = 0;
+
+  printf("ARRIVAL_RATE = %f\tSERVICE_TIME = %d\tMAX_QUEUE_SIZE =%d\n", arrival_rate, service_time, max_queue_size);
+  //printf("SERVICE_TIME = %d\n", service_time);
+  
+  for(int run = 0; run < 10; run++){
+    double clock = 0; /* Clock keeps track of simulation time. */
+
+    /* System state variables. */
+    int number_in_system = 0;
+    double next_arrival_time = 0;
+    double next_departure_time = 0;
+
+    /* Data collection variables. */
+    long int total_served = 0;
+    long int total_arrived = 0;
+    long int total_rejected = 0;
+
+    double total_busy_time = 0;
+    double integral_of_n = 0;
+    double last_event_time = 0;
+
+    /* Set the seed of the random number generator. Will use McMaster ID as the random seed for a single run*/
+    random_generator_initialize(random_seeds[run]);
+
+    /* Process customers until we are finished. */
+    while (total_served < number_to_serve) {
+        /* Test if the next event is a customer arrival or departure. */
+      if(number_in_system == 0 || next_arrival_time < next_departure_time) {
+
+        /*
+        * A new arrival is occurring.
+        */
+
+        clock = next_arrival_time;
+        next_arrival_time = clock + exponential_generator((md1_or_mm1) ? (double)service_time : (double) 1/arrival_rate); //Step 8
+
+        /* Update our statistics. */
+        integral_of_n += number_in_system * (clock - last_event_time);
+        last_event_time = clock;
+
+        //Step 7
+        if(use_max_queue == 1 && number_in_system > max_queue_size) {
+          total_rejected++;
+        } else {
+          number_in_system++;
+        }
+        
+        total_arrived++;
+
+        /* If this customer has arrived to an empty system, start its
+        service right away. */
+        if(number_in_system == 1) next_departure_time = clock + service_time;
+        
+    
+      } else {
+
+        /*
+          * A customer departure is occuring. 
+          */
+
+        clock = next_departure_time;
+
+        /* Update our statistics. */
+        integral_of_n += number_in_system * (clock - last_event_time);
+        last_event_time = clock;
+
+        number_in_system--;
+        total_served++;
+        total_busy_time += service_time;
+
+        /* 
+          * If there are other customers waiting, start one in service
+          * right away.
+          */
+
+        if(number_in_system > 0) next_departure_time = clock + service_time;
+
+        /* 
+          * Every so often, print an activity message to show we are active. 
+          */
+
+        if (total_served % BLIP_RATE == 0)
+          printf("Customers served = %ld (Total arrived = %ld)\r",
+            total_served, total_arrived);
+      }
+
+    }
+
+    acc_utilization += total_busy_time/clock;
+    acc_fraction_served += (double) total_served/total_arrived;
+    acc_rejection_probability += (double) total_rejected/total_arrived;
+    acc_mean_number_in_system += integral_of_n/clock;
+    acc_mean_delay += integral_of_n/total_served;
+
+    printf("RESULTS AFTER RUN #%d - utilization = %f, fraction_served = %f, rejection_probability = %f, mean_number_in_system = %f, mean_delay = %f\n",
+      run, total_busy_time/clock, (double) total_served/total_arrived, (double) total_rejected/total_arrived, integral_of_n/clock, integral_of_n/total_served);
+
+  }
+
+  printf("AVERAGED RESULTS - utilization = %f, fraction_served = %f, rejection_probability = %f, mean_number_in_system = %f, mean_delay = %f\n",
+    acc_utilization/10, acc_fraction_served/10, (double) acc_rejection_probability/10, acc_mean_number_in_system/10, acc_mean_delay/10);
+
+  printf("\n");
+
+  return (Simulation_Result){
+    arrival_rate,
+    service_time,
+    number_to_serve,
+    max_queue_size,
+
+    acc_utilization/10,
+    acc_fraction_served/10,
+    acc_rejection_probability/10,
+    acc_mean_number_in_system/10,
+    acc_mean_delay/10
+  };
+}
+
+int flush_results(char* step_name, Simulation_Result *sim_rslts, int sim_rslts_size, FILE *fpt){
+  // export file here
+  fprintf(fpt, "\n%s\n", step_name);
+
+  printf("\nRESULTS!\n");
+  for (int i=0; i<sim_rslts_size; i++) {
+    fprintf(fpt, "%f, %d, %f, %f, %f, %f, %f, %f\n",
+      sim_rslts[i].arrival_rate,
+      sim_rslts[i].service_time,
+      sim_rslts[i].number_to_serve,
+      sim_rslts[i].utilization,
+      sim_rslts[i].fraction_served,
+      sim_rslts[i].rejection_probability,
+      sim_rslts[i].mean_number_in_system,
+      sim_rslts[i].mean_delay
+      );
+
+    // printf("utilization = %f\n", sim_rslts[i].utilization);
+    // printf("fraction_served = %f\n", sim_rslts[i].fraction_served);
+    // printf("mean_number_in_system = %f\n", sim_rslts[i].mean_number_in_system);
+    // printf("mean_delay = %f\n", sim_rslts[i].mean_delay);
+  }
+
+  
+  return 0;
+}
 /*******************************************************************************/
 
 /*
@@ -53,90 +422,16 @@
 
 int main()
 {
-  double clock = 0; /* Clock keeps track of simulation time. */
-
-  /* System state variables. */
-  int number_in_system = 0;
-  double next_arrival_time = 0;
-  double next_departure_time = 0;
-
-  /* Data collection variables. */
-  long int total_served = 0;
-  long int total_arrived = 0;
-
-  double total_busy_time = 0;
-  double integral_of_n = 0;
-  double last_event_time = 0;
-
-  /* Set the seed of the random number generator. */
-  random_generator_initialize(RANDOM_SEED);
-
-  /* Process customers until we are finished. */
-  while (total_served < NUMBER_TO_SERVE) {
-
-    /* Test if the next event is a customer arrival or departure. */
-   if(number_in_system == 0 || next_arrival_time < next_departure_time) {
-
-     /*
-      * A new arrival is occurring.
-      */
-
-     clock = next_arrival_time;
-     next_arrival_time = clock + exponential_generator((double) 1/ARRIVAL_RATE);
-
-     /* Update our statistics. */
-     integral_of_n += number_in_system * (clock - last_event_time);
-     last_event_time = clock;
-
-     number_in_system++;
-     total_arrived++;
-
-     /* If this customer has arrived to an empty system, start its
-	service right away. */
-     if(number_in_system == 1) next_departure_time = clock + SERVICE_TIME;
-
-   } else {
-
-     /*
-      * A customer departure is occuring. 
-      */
-
-     clock = next_departure_time;
-
-     /* Update our statistics. */
-     integral_of_n += number_in_system * (clock - last_event_time);
-     last_event_time = clock;
-
-     number_in_system--;
-     total_served++;
-     total_busy_time += SERVICE_TIME;
-
-     /* 
-      * If there are other customers waiting, start one in service
-      * right away.
-      */
-
-     if(number_in_system > 0) next_departure_time = clock + SERVICE_TIME;
-
-     /* 
-      * Every so often, print an activity message to show we are active. 
-      */
-
-     if (total_served % BLIP_RATE == 0)
-       printf("Customers served = %ld (Total arrived = %ld)\r",
-	      total_served, total_arrived);
-   }
-
-  }
-
-  /* Output final results. */
-  printf("\nUtilization = %f\n", total_busy_time/clock);
-  printf("Fraction served = %f\n", (double) total_served/total_arrived);
-  printf("Mean number in system = %f\n", integral_of_n/clock);
-  printf("Mean delay = %f\n", integral_of_n/total_served);
+  run(steps, 6);
+  //printf("Steps name = %s", steps[0].step_name);
+  // /* Output final results. */
+  // printf("\nUtilization = %f\n", total_busy_time/clock);
+  // printf("Fraction served = %f\n", (double) total_served/total_arrived);
+  // printf("Mean number in system = %f\n", integral_of_n/clock);
+  // printf("Mean delay = %f\n", integral_of_n/total_served);
 
   /* Halt the program before exiting. */
-  printf("Hit Enter to finish ... \n");
+  printf("\nHit Enter to finish ... \n");
   getchar(); 
 
   return 0;
